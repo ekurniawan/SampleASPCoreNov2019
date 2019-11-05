b1d95eaf3c429d85f99dba8e19920e277593efd2
Erick Kurniawan <erick.kurniawan@gmail.com>
2019-11-05 10:38:08 +0700

    menambahkan restaurant model

--- Controllers/HomesController.cs
+++ Controllers/HomesController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using System;
 
 namespace SampleASPCore.Controllers
 {
@@ -10,10 +11,25 @@ namespace SampleASPCore.Controllers
             return View();
         }
 
+        [HttpPost]
         public IActionResult IndexHitung(double bil1, double bil2)
         {
-            double hasil = bil1*bil2;
-            return Content($"Hasilnya : {hasil}");
+            try
+            {
+                double hasil = bil1*bil2;
+
+                ViewBag.bil1 = bil1;
+                ViewBag.bil2 = bil2;
+                ViewData["hasil"] = hasil;
+                return View();
+            }
+            catch (Exception ex)
+            {
+                return Content($"{ex.Message}");
+            }
+            
+            
+           
         }
     }
 
