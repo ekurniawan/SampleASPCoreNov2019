95da663962fc106b151e9bcc939715ed1e32f547
Erick Kurniawan <erick.kurniawan@gmail.com>
2019-11-05 15:53:57 +0700

    menambahkan tag helper

---
+++ TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleASPCore.TagHelpers
+{
+    public class EmailTagHelper: TagHelper
+    {
+        private const string EmailDomain = "actual-training.com";
+        public string MailTo { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a"; // Replaces <email> with <a> tag
+            var address = MailTo + "@" + EmailDomain;
+            output.Attributes.SetAttribute("href", "mailto:" + address);
+            output.Content.SetContent(address);
+        }
+    }
+}
