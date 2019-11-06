0b164da11f0b08634dcb46a7287101166b018670
Erick Kurniawan <erick.kurniawan@gmail.com>
2019-11-06 08:34:45 +0700

    menambahkan connection string

---
+++ Data/RestaurantDAL.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SampleASPCore.Models;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace SampleASPCore.Data
+{
+    public class RestaurantDAL : IRestaurantData
+    {
+        private IConfiguration _config;
+        public RestaurantDAL(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        private string GetConnectionString()
+        {
+            return _config.GetConnectionString("DefaultConnection");
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Restaurant GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
